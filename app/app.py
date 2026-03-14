ba2b57622fee4494e67cabba26583d2ed99cfae5
taniken <48510381+taniken-dev@users.noreply.github.com>
2026-03-15 01:45:52 +0900

    Update app.py

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-
+db.init_app(app)
 with app.app_context():
     db.create_all()
 
